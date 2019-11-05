059f5769c95ebb3bad5fc4829ad9639d4a6d2ecd
aishamu <aishamutip@hotmail.com>
2019-11-05 22:54:21 +0300

    second project

--- src/BookCard.js
+++ src/BookCard.js
@@ -1,30 +1,26 @@
 import React from 'react'
 import './App.css'
- 
-// import { Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
-// import { Card } from 'react-bootstrap'
-// import Row from 'react-bootstrap/Row'
+
 const BookCard = (props) =>{
  
     return(
 <div className="card-contrainer">
 <div className="p-3">
 
-<img src={props.image} alt=""/>
-<br/>
-<br/>
-  <h4>{props.title}</h4>
-  <h6>{props.desc}</h6>
-  <h6>{props.author}</h6>
-  <h6>{props.published}</h6>
-  
+   <img src={props.image} alt=""/>
+   <br/>
+   <br/>
+   <h5>{props.title}</h5>
+   <br/>
+   <h6 className="h"><b>Category:</b> {props.desc}</h6>
+   <h6 className="h"><b>Author:</b> {props.author}</h6>
+   <h6 className="h"><b>Published:</b> {props.published}</h6>
+   <br/>
    <section className="box"> <button className="download"><a href= {props.link}>Open a book </a></button>
    <button className="download"><a href= {props.download}>Downlod it </a></button>
    </section> 
  
 </div>
- 
-
 </div>
          )
 
