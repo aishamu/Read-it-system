e6c5a932db7bd5fc3eedccf3ee5c41f567e59a0d
aishamu <aishamutip@hotmail.com>
2019-11-06 16:41:24 +0300

    react-create-app on Heroku

--- src/Abouts.js
+++ src/Abouts.js
@@ -12,17 +12,30 @@ export default class Abouts extends Component {
         {/* <h2>Read It </h2> */}
   
   
-        <h5>
+        <h5 className="h05">
         Read it is a social cataloging website that allows individuals to freely search of books , open it or download it
  </h5>
       </div>
-      <div className="col-sm-6 text-picture__picture">
-        <img src="https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=14cc613995f60d89c4a908f3ea5c2409&auto=format&fit=crop&w=500&q=60" width="80%"alt="Image" alt="" className="img-responsive" />
-      </div>
+ 
+       <div class="grid-container">
+            <div class="grid-image-container">
+              <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" width="100%" height="100%" alt="pic1"/>
+            </div>
+            <div class="grid-image-container">
+              <img src="https://images.unsplash.com/photo-1470549638415-0a0755be0619?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" width="100%" height="100%" alt="pic2"/>
+            </div>
+            <div class="grid-image-container"> 
+              <img src="https://images.unsplash.com/photo-1560693478-dfdb32f2176a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" width="100%" height="100%" alt="pic3"/> 
+            </div>
+            <div class="grid-image-container">
+              <img src="https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80" width="100%" height="100%" alt="pic4"/> 
+            </div>
+          </div>
+        
     </div>
   </div>
 </div>
-
+<br/>   <br/>      <br/>   <br/>     
 
           
             </div>
